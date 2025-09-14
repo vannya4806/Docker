e196ed86437ba96b3a3a9d017f619cdbadd8d1d0
Vannya Ade Gunawan <vannyaade@student.ub.ac.id>
2025-09-14 11:50:49 +0700

    Update train.py

--- src/train.py
+++ src/train.py
@@ -3,13 +3,19 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 import pickle
 import os
+from preprocess import preprocess_data
 
 # Load dataset
 df = pd.read_csv("data/personality_dataset.csv")
 
+# Preprocess
+df = preprocess_data(df, training=True)
+
+# Split features & target
 X = df.drop("Personality", axis=1)
 y = df["Personality"]
 
+# Train test split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Train model
