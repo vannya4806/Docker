9239ca010a1a33024d5f7aa9a2ed87f1fd418f39
Vannya Ade Gunawan <vannyaade@student.ub.ac.id>
2025-09-14 14:25:44 +0700

    Update predict.py

--- src/predict.py
+++ src/predict.py
@@ -9,9 +9,3 @@ def load_model():
 def predict(input_data):
     model = load_model()
     return model.predict([input_data])
-
-if __name__ == "__main__":
-    # Contoh input (ubah sesuai datasetmu)
-    sample_input = [0, 1, 2, 3, 4]
-    prediction = predict(sample_input)
-    print("Prediksi:", prediction)
