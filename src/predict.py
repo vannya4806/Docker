e34879e7c5f420848949f22f7c9f0bf2d7448ba7
Vannya Ade Gunawan <vannyaade@student.ub.ac.id>
2025-09-14 12:07:11 +0700

    Update predict.py

--- src/predict.py
+++ src/predict.py
@@ -5,5 +5,5 @@ def predict(input_data):
     Prediksi menggunakan model yang sudah disimpan.
     input_data: numpy array atau list (1 sample atau banyak sample)
     """
-    model = joblib.load("models/model.pkl")
+    model = joblib.load("src/model.pkl")
     return model.predict(input_data)
