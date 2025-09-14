667a81de39021d1d60ed43b5dac35c283df4847f
Vannya Ade Gunawan <vannyaade@student.ub.ac.id>
2025-09-14 11:43:24 +0700

    Update test_prediction.py

--- tests/test_prediction.py
+++ tests/test_prediction.py
@@ -1,6 +1,11 @@
-from src.predict import predict_personality
+import pickle
+import numpy as np
 
-def test_prediction():
-    sample_input = [5, 0, 6, 4, 1, 10, 5]
-    result = predict_personality(sample_input)
-    assert result in ["Introvert", "Extrovert"]
+def test_single_prediction():
+    with open("models/model.pkl", "rb") as f:
+        model = pickle.load(f)
+
+    sample = np.array([23, 1, 6.5, 5.1, 7.0, 6.2, 5.8]).reshape(1, -1)
+    pred = model.predict(sample)
+
+    assert pred is not None
