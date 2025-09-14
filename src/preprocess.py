fda09cbe2e052277b07a20ce4a6c870d9421fb38
Vannya Ade Gunawan <vannyaade@student.ub.ac.id>
2025-09-14 14:19:02 +0700

    Update preprocess.py

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,7 +1,7 @@
 from sklearn.model_selection import train_test_split
 from sklearn.datasets import load_iris
 
-def load_data(test_size=0.2, random_state=42):
+def load_and_preprocess(test_size=0.2, random_state=42):
     """
     Contoh fungsi load dataset Iris dari sklearn.
     Return: X_train, X_test, y_train, y_test
