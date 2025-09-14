7e27ef72910072f30af27599044385a8858699e4
Vannya Ade Gunawan <vannyaade@student.ub.ac.id>
2025-09-14 12:15:01 +0700

    Delete src/model.py

--- src/model.py
+++
@@ -1,9 +0,0 @@
-from sklearn.linear_model import LogisticRegression
-
-def build_model():
-    """
-    Membuat dan mengembalikan instance model machine learning.
-    Ganti LogisticRegression dengan model lain kalau perlu.
-    """
-    model = LogisticRegression(max_iter=1000, random_state=42)
-    return model
