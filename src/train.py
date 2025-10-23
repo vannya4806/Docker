9000fee68feda789af5c645365dc300959b2cf0c
Vannya Ade Gunawan <vannyaade@student.ub.ac.id>
2025-10-23 20:05:58 +0700

    Update train.py

--- src/train.py
+++ src/train.py
@@ -5,8 +5,7 @@ import pickle
 import os
 if __name__ == "__main__":
   # Load & preprocess data
-  X_train, X_test, y_train, y_test =
-  load_and_preprocess("personality_dataset.csv")
+  X_train, X_test, y_train, y_test = load_and_preprocess("personality_dataset.csv")
   # Buat model langsung
   model = LogisticRegression(max_iter=1000, random_state=42)
   model.fit(X_train, y_train)
