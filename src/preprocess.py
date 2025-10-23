656cc5841b1e0286fc25475d2731b77cf8c5efd7
Vannya Ade Gunawan <vannyaade@student.ub.ac.id>
2025-10-23 20:08:33 +0700

    Update preprocess.py

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,14 +1,14 @@
 from sklearn.model_selection import train_test_split
 from sklearn.datasets import load_iris
 def load_and_preprocess(test_size=0.2, random_state=42):
-"""
-Contoh fungsi load dataset Iris dari sklearn.
-Return: X_train, X_test, y_train, y_test
-"""
-data = load_iris()
-X = data.data
-y = data.target
-X_train, X_test, y_train, y_test = train_test_split(
-X, y, test_size=0.2, random_state=None
-)
-return X_train, X_test, y_train, y_test
+  """
+  Contoh fungsi load dataset Iris dari sklearn.
+  Return: X_train, X_test, y_train, y_test
+  """
+  data = load_iris()
+  X = data.data
+  y = data.target
+  X_train, X_test, y_train, y_test = train_test_split(
+  X, y, test_size=0.2, random_state=None
+  )
+  return X_train, X_test, y_train, y_test
